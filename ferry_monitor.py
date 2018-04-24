e56da931dd6bdae58796bd78a05829dcaff6dffe
kenfox0115 <ken.c.fox@gmail.com>
2018-04-24 09:31:58 -0700

    Update ferry_monitor.py

--- ferry_monitor.py
+++ ferry_monitor.py
@@ -1,11 +1,5 @@
-import requests
-import json
-
-#set schedule variable for boats going SF->Vallejo
-sched = requests.get('http://seaswimdash.marsec.services/feed/departures?numberOfDays=2&route=VAL-SF&route=VAL-Pier41&route=Pier41-SF')
-
-#set variable for parsed json version of lineup 
-sched_json = sched.json()
-
-#print json object
-print sched_json
+import urllib, json
+url = "http://seaswimdash.marsec.services/feed/departures?numberOfDays=2&route=VAL-SF&route=VAL-Pier41&route=Pier41-SF"
+response = urllib.urlopen(url)
+sched_dict = (json.loads(response.read()))
+print sched_dict
